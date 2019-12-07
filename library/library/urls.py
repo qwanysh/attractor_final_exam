7741ce134bbb5834949cce3f25ba27141d007ab4
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 17:37:31 +0600

    Добавлено скачивание файла книги

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('', include('books.urls', namespace='books')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
