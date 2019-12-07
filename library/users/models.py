42321bf4102946579fdcb4f43e3f0c912e7fc85f
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 11:26:38 +0600

    Добавлена модель User

--- library/users/models.py
+++ library/users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    date_of_birth = models.DateField(
+        verbose_name='Date of birth',
+        null=True,
+        blank=True,
+        default=None
+    )
+    date_of_death = models.DateField(
+        verbose_name='Date of death',
+        null=True,
+        blank=True,
+        default=None
+    )
+    biography = models.TextField(
+        verbose_name='Biography',
+        max_length=500,
+        null=True,
+        blank=True,
+        default=None
+    )
+    image = models.ImageField(
+        verbose_name='Image',
+        null=True,
+        blank=True,
+        default=None,
+        upload_to='user_images',
+    )
+
+    def __str__(self):
+        return self.username
