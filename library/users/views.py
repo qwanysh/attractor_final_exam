a9fd1e725303cfb11e903dbe0f228ae75027cfce
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 11:35:32 +0600

    Добавлен UserDetailView и директорий templates с базовым шаблоном

--- library/users/views.py
+++ library/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import DetailView
+from .models import User
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user/detail.html'
+
