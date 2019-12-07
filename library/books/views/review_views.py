475508ed50c4285afbc76b65d7477c4231d254e1
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 17:18:42 +0600

    Добавлено разграничение прав доступа для Review

--- library/books/views/review_views.py
+++ library/books/views/review_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import View
@@ -6,7 +7,7 @@ from ..forms import ReviewCreateForm
 from ..models import Review, Book
 
 
-class ReviewCreateView(View):
+class ReviewCreateView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         form = ReviewCreateForm(data=request.POST)
         if form.is_valid():
@@ -16,8 +17,12 @@ class ReviewCreateView(View):
         return redirect(reverse('books:book_detail', kwargs={'pk': kwargs.get('book_pk')}))
 
 
-class ReviewDeleteView(View):
+class ReviewDeleteView(UserPassesTestMixin, View):
     def get(self, request, *args, **kwargs):
         review = get_object_or_404(Review, pk=kwargs.get('pk'))
         review.delete()
         return redirect(reverse('books:book_detail', kwargs={'pk': kwargs.get('book_pk')}))
+
+    def test_func(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('pk'))
+        return review.user == self.request.user
