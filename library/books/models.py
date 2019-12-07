3eb9197bc3687c495ab29ed1faa4ccd52082a198
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 11:53:19 +0600

    Добавлена модель Author

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        default=None,
+        verbose_name='Full name'
+    )
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Date of birth'
+    )
+    date_of_death = models.DateField(
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Date of death'
+    )
+    biography = models.TextField(
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Biography'
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        default=None,
+        upload_to='author_images',
+        verbose_name='Image'
+    )
+
+    def __str__(self):
+        return self.full_name
