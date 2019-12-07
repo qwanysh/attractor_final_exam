3d5738cdb00acf3544d80e35dd9d7aed995a464b
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 13:43:48 +0600

    Добавлена модель книги

--- library/books/models.py
+++ library/books/models.py
@@ -35,3 +35,48 @@ class Author(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+class Book(models.Model):
+    author = models.ForeignKey(
+        'books.Author',
+        on_delete=models.PROTECT,
+        verbose_name='Author'
+    )
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        default=None,
+        verbose_name='Name'
+    )
+    year_of_publish = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        default=None,
+        verbose_name='Year of publish'
+    )
+    file = models.FileField(
+        null=True,
+        blank=True,
+        default=None,
+        upload_to='book_files',
+        verbose_name='File'
+    )
+    coverage = models.FileField(
+        null=True,
+        blank=True,
+        default=None,
+        upload_to='book_coverages',
+        verbose_name='Coverage'
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        default=None,
+        verbose_name='Description'
+    )
+
+    def __str__(self):
+        return self.name
