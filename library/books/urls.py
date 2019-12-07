68f1299f6f4076a9f348fe766af75d3ddf0a1b2f
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 12:02:05 +0600

    Добавлен url для AuthorListView

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import AuthorListView
 
 app_name = 'books'
 
 urlpatterns = [
-
+    path('authors/<int:pk>/', AuthorListView.as_view(), name='author_list'),
 ]
