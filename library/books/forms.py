a93eb8b75b7b91c445453d1940ed4187f1faf567
Beisembayev Kuanysh <olimpsdu@gmail.com>
2019-12-07 12:44:55 +0600

    Добавлены стили для формы создания автора

--- library/books/forms.py
+++ library/books/forms.py
@@ -1,9 +1,35 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Author
 
 
-class AuthorCreateForm(ModelForm):
+class AuthorCreateForm(forms.ModelForm):
+    full_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    date_of_birth = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    date_of_death = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    biography = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control'
+        })
+    )
+    image = forms.ImageField(
+        widget=forms.FileInput(attrs={
+            'class': 'form-control'
+        })
+    )
+
     class Meta:
         model = Author
         exclude = []
--- library/books/views/author_views.py
+++ library/books/views/author_views.py
@@ -1,11 +1,16 @@
 from django.views.generic import ListView, DetailView
 
 from ..models import Author
+from ..forms import AuthorCreateForm
 
 
 class AuthorListView(ListView):
     model = Author
     template_name = 'author/list.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['form'] = AuthorCreateForm()
+        return super().get_context_data(**kwargs)
 
 
 class AuthorDetailView(DetailView):
